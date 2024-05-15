882893998024b0ce9b7a71e5d65ac0c53d0da8af
Joe Lyles <lylesjoe@protonmail.com>
2024-05-15 04:51:07 -0400

    hero section and tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ const Navbar = () => {
         <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div className="flex h-20 items-center justify-between">
                 <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+                    <span className="hidden md:block text-white text-2xl font-bold ml-2">React Jobs</span>
                     <div className="md:ml-auto">
                         <div className="flex space-x-2">
                             <a href="" className="text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2">Home</a>
@@ -13,7 +14,6 @@ const Navbar = () => {
                 </div>
             </div>
         </div>
-      
     </nav>
   )
 }
